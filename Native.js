3ac30c222526d2fd97514a438bd392d50b2943f4
kim038 <kimc074@newschool.edu>
2025-10-08 00:43:47 -0400

    Update

--- Native.js
+++ Native.js
@@ -1,9 +1,3 @@
-var hoverArea = document.getElementById('audioone');
-var audio = document.getElementById('callone');
-hoverArea.onmouseover = function () {
-    audio.play();
-}
-hoverArea.onmouseout = function () {
-    audio.pause();
-}
+
+
 
